a1e9f3cd7604e5ce69e16ea6c9d4e8217ba4ef97
Christian Bläul <blaeul@quodata.de>
2014-05-02 21:57:07 +0200

    compare.php erstmalig auf Intranet02 zum Laufen gebracht

--- html/compare.php
+++ html/compare.php
@@ -2,14 +2,20 @@
 
 $sTestName = 'download-seite';
 if (isset($_GET['sTestName']))
-    $sTestName = $_GET['sTestName'];
+    $sTestName = str_replace('/', '\\', $_GET['sTestName']);
+	
+$sStem = substr($sTestName, 0, -8);
+$sExt = substr($sTestName, -3);
+$sFileIst = "$sStem-ist.$sExt";
+$sFileSoll = "$sStem-soll.$sExt";
+
 $sCompare = '"C:\\Program Files\\ImageMagick-6.8.9-Q16\\compare.exe"';
-$sCmd = "$sCompare -compose src $sTestName-ist.png $sTestName-soll.png difference.png";
+$sCmd = "$sCompare -compose src $sFileIst $sFileSoll Bilder\\difference.png";
 $sRetVal = `$sCmd 2>&1`;
 
 if (trim($sRetVal) == '') {
     header("Content-Type: image/png");
-    readfile('difference.png');
+    readfile('Bilder/difference.png');
 } else {
     echo "<tt>$sCmd</tt><br><b style='color:red'>$sRetVal </b>";
 }
