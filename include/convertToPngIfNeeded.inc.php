65fc0faad3c69639b8ea8515e22125ae3f875e6b
Christian Bläul <blaeul@quodata.de>
2014-06-04 09:58:43 +0200

    Code zur Konvertierung von BMP zu PNG hinzu

---
+++ include/convertToPngIfNeeded.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+function convertToPngIfNeeded($sName, &$sExt) {
+  if (stristr($sExt, 'png')) return $sName . '.png';
+
+  if (file_exists($sName . '.png')) unlink($sName . '.png');
+
+  if (stristr($sExt, 'bmp')) {
+    require_once('../include/ImageCreateFromBMP.inc.php');
+    $res=ImageCreateFromBMP($sName . '.bmp');
+    if (!is_resource($res)) die('ImageCreateFromBMP failed with ' . $res);
+    imagepng($res, $sName . '.png');
+    $sRetVal ='';
+  } else {
+    $sConvert = '"C:\\Program Files\\ImageMagick-6.8.9-Q16\\convert.exe"';
+    $sPath = dirname($sName);
+    $sBaseName = basename($sName);
+    $sCmd = "cd $sPath && ";
+    $sMagic = stristr($sExt, 'bmp') ? 'DIB:' : '';
+    $sFile = "\"$sMagic$sBaseName.$sExt\"";
+    echo $sMagic;
+    $sCmd .= "$sConvert $sFile \"$sBaseName.png\"";
+  #die( "$sCmd<hr>");
+    $sRetVal = `$sCmd 2>&1`;
+  }
+  if (file_exists($sName . '.png') && trim($sRetVal) == '') {
+    unlink("$sName.$sExt");
+    $sExt = 'png';
+    echo "I did convert $sName<hr>";
+    return "$sName.png";
+  }
+
+  die("<tt>$sCmd</tt><br><b style='color:red'>$sRetVal </b>");
+}
