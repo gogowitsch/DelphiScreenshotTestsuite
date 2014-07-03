63866ecbf6a7afd032f0b1b148c7bdb1465d88b9
Screenshot01-PC <test@quodata.de>
2014-07-03 11:10:39 +0200

    Starten von Tests per Browser, weitere Projekte hinzu

--- include/projectstatus.inc.php
+++ include/projectstatus.inc.php
@@ -1,25 +1,35 @@
 <?php
 
-function getProjectStatus($sProject, $p_sExePath) {
-	global $sExePath, $iExeTime, $aTests, $aProjects, $iStatusSum, $iLocalStatusSum, $aNewTests;
+function getProjectStatus($sProject, $p_sExePath, $sCmd = '') {
+  global $sExePath, $iExeTime, $aTests, $aProjects, $iStatusSum, $iLocalStatusSum, $aNewTests;
+  global $smarty;
 
   if (isset($_GET['project']) && $_GET['project']!=$sProject) return;
 
+  if (!empty($_GET['run'])) {
+    if (!empty($sCmd)) {
+      $_GET['message'] = "Kommandozeile '$sCmd' wurde ausgef&uuml;hrt. " . `$sCmd`;
+    } else {
+      $_GET['message'] = 'Fuer dieses Projekt wurde keine Kommandozeile hinterlegt.';
+    }
+  }
+
   $sPicturePath = "$sProject/";
-	$sExePath = $p_sExePath;
-	$iExeTime = filemtime($sExePath);
+  $sExePath = $p_sExePath;
+  $iExeTime = filemtime($sExePath);
   $aNewTests = array();
   $iLocalStatusSum = 0;
-	array_walk(glob("Bilder/$sPicturePath*-ist.???"), function($sFile) {
-		global $aNewTests, $iLocalStatusSum;
-		$aNewTests[] = $aTest = getScreenshotStatus($sFile);
-		$iLocalStatusSum += $aTest['status'];
-	});
+  array_walk(glob("Bilder/$sPicturePath*-ist.???"), function($sFile) {
+    global $aNewTests, $iLocalStatusSum;
+    $aNewTests[] = $aTest = getScreenshotStatus($sFile);
+    $iLocalStatusSum += $aTest['status'];
+  });
 
   $aProjects[]  = array(
     'title' => $sProject,
     'status' => $iLocalStatusSum==count($aNewTests) ? 1 : 0,
     'ratio' => $iLocalStatusSum . " / " . count($aNewTests),
+    'cmd' => $sCmd
   );
   $aTests = array_merge($aTests, $aNewTests);
   $iStatusSum += $iLocalStatusSum;
@@ -28,6 +38,12 @@ function getProjectStatus($sProject, $p_sExePath) {
 function getStatusOfAllProjects() {
   global $aTests;
   $aTests = array();
-  getProjectStatus('PROLab_de', '\\\\delphicompiler0\\prolab_plus_de_AD\\PROLab_de.exe');
-  getProjectStatus('PROLab_en', '\\\\delphicompiler0\\prolab_plus_en_AD\\PROLab_en.exe');
+  $sAhkCmd = '"C:\\Program Files\\AutoHotkey\\AutoHotkey.exe" /ErrorStdOut ';
+  $sAhkFolderPl = 'C:\\Users\\Screenhot01\\Desktop\\ScreenshotsPROLab\\Test starten -';
+  getProjectStatus('PROLab_de', 'c:/daten/prolab_plus_de_AD\\PROLab_de.exe', "$sAhkCmd \"$sAhkFolderPl PROLab_de.ahk\"");
+  getProjectStatus('PROLab_en', 'c:/daten/prolab_plus_en_AD\\PROLab_en.exe', "$sAhkCmd \"$sAhkFolderPl PROLab_en.ahk\"");
+  getProjectStatus('mqVAL_DE', 'c:/daten/mqVAL_DE\\mqVAL.exe', "$sAhkCmd \"$sAhkFolderPl mqVAL_DE.ahk\"");
+  getProjectStatus('PROLab_Smart_DE', 'c:/daten/PROLab_Smart_DE\\PROLabSmart.exe', "$sAhkCmd \"$sAhkFolderPl PROLab_Smart_DE.ahk\"");
+  // getProjectStatus('PROLab_Smart_EN', 'c:/daten/PROLab_Smart_EN\\PROLabSmart.exe', "$sAhkCmd \"$sAhkFolderPl PROLab_Smart_EN.ahk\"");
 }
+// `start calc`;
