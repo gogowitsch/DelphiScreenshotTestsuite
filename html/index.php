70eb6a8b57dc55c6dfc625edb68917afa189407b
Peter Oertel <peter.oertel@quodata.de>
2015-10-16 11:49:35 +0200

    DelphiScreenshotTestsuite: Job-Warteschlange mit E-Mail Abfrage erstellt. TODO: E-Mail an Nutzer nach beenden eines Jobs wirft noch phpmailer exception und E-Mail soll noch mehr Informationen enthalten.

--- html/index.php
+++ html/index.php
@@ -3,6 +3,9 @@
 require '../include/smarty.inc.php';
 require '../include/screenshot.inc.php';
 require '../include/projectstatus.inc.php';
+require '../include/queue.inc.php';
+
+session_start();
 
 $aTests = array();
 $aProjects = array();
@@ -10,6 +13,25 @@ $iStatusSum = 0;
 
 getStatusOfAllProjects();
 
+// Abgeschlossene Jobs löschen und neuen starten
+if (!empty($_GET['job_done'])) {
+    check_queue();
+}
+
+//Job in in Job-Liste(DB) schreiben
+$bEmail = false;
+if (!empty($_POST["email"])) {
+    save_job($aProjects);
+    $bEmail = true;
+}
+
+$sEmail = "";
+if (!empty($_SESSION['email'])) {
+    $sEmail = $_SESSION['email'];
+}
+
+$smarty->assign("sEmail", $sEmail);
+$smarty->assign("bEmail", $bEmail);
 $smarty->assign("aProjects", $aProjects);
 $smarty->assign("bHasHiddenProjects", 0);
 $smarty->assign("aTests", $aTests);
