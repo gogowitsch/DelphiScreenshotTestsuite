3100a9b08ba1c70d72b897a964b9da69b5967ebc
Christian Bläul <blaeul@quodata.de>
2016-03-24 11:07:02 +0100

    GitLab-push-hook.php verbessert:  - Branchdatei enthält den echten Namen des Branches  - die curl-Ausgabe wird per Mail verschickt wird, falls etwas nicht klappt

--- html/GitLab-push-hook.php
+++ html/GitLab-push-hook.php
@@ -1,52 +1,53 @@
 <?php
 
-// hier müssen alle Repos eingetragen werden, die dieses Skript verarbeiten kann:
-if ($_REQUEST['repo'] == 'ringdat-online')
-  chdir("C:\\xampp\\htdocs\\lvu\\tests\\PhantomJS");
-
-// GIT verbieten Nutzer nach Eingaben zu fragen:
-putenv("GIT_ASK_YESNO=false");
-
-// Das folgende geht, weil ein Nutzer �ber den Cred Helper f�r den Nutzer des Webservers eingetragen wurde.
 $iErrorLevel = 0;
-$sDatei = "Alter_des_Masterbranches.txt";
-if(!file_exists($sDatei))
-  $iErrorLevel = 2;
-else {
-  $data = json_decode(file_get_contents('php://input'), true);
-  if ($data['ref'] == 'refs/heads/reviewed-code-for-screenshots') {
+$data = json_decode(file_get_contents('php://input'), true);
+
+// hier müssen alle Repos eingetragen werden, die dieses Skript verarbeiten kann:
+if (basename($data['repository']['url']) == 'rdo.git')
+    chdir("C:\\xampp\\htdocs\\lvu\\tests\\PhantomJS");
+
+$sBranch = basename($data['ref']); // Beispielwert ist 'refs/heads/master'
+$sDatei = "Alter_des_Branches-$sBranch.txt";
+if (true || $sBranch == 'reviewed-code-for-screenshots') {
     file_put_contents($sDatei, strftime('%c'));
     file_put_contents($sDatei, print_r($data, true), FILE_APPEND);
     $iStamp = 0;
-    foreach($data['commits'] as $aCommit) {
-      $iStamp = max($iStamp, strtotime($aCommit['timestamp']));
+    foreach ($data['commits'] as $aCommit) {
+        $iStamp = max($iStamp, strtotime($aCommit['timestamp']));
     }
-    if ($iStamp)
-      touch($sDatei, $iStamp);
-    `curl.exe -o - "http://localhost/run_project.php?project=RingDat_Online.IBBL&run=1"`;
-    #sleep(5);
-    #`curl.exe -o - "http://localhost/run_project.php?project=RingDat_Online.InstitutEignungspruefung&run=1"`;
-  }
+    if ($iStamp) {
+        // wenn der Rechner nicht beim Push/Merge verfügbar ist, wird
+        // der Hook periodisch wiederholt. Was für die Testaktualität zählt,
+        // ist die Commit-Zeit $iStamp, nicht die Ausführungszeit des Skriptes
+        touch($sDatei, $iStamp);
+        $sCmd = 'curl.exe -o - "http://localhost/run_project.php?project=RingDat_Online.IBBL&run=1"';
+        $result = system("$sCmd 2>&1", $iErrorLevel);
+        file_put_contents($sDatei, $result, FILE_APPEND);
+        #sleep(5);
+        #`curl.exe -o - "http://localhost/run_project.php?project=RingDat_Online.InstitutEignungspruefung&run=1"`;
+    }
 }
 
-if ($iErrorLevel > 0) {
-  $sSubj = basename(__FILE__) . ': error';
-  $sMsg = "An error occured while running the file " .
-    __FILE__ . ":\n\n" .
-    $sCmd . "\n\n" . $result . "\n\n" .
-    "Repo: $_REQUEST[repo] - " . `echo %cd%`;
 
-  $sHeader = "From: " . basename(__FILE__) . '@quodata.de';
-  $aRecip = array(
-    'Blaeul@quodata.de',
-    'Oertel@quodata.de',
-    'Sgorzaly@quodata.de',
-    'Pham@quodata.de',
-  );
-  $sMsg .= "\nWeitere Details gibt es unter https://git04.quodata.de/it/$_REQUEST[repo].\n\n";
-  $sMsg .= "Diese E-Mail ging an " . str_replace("@quodata.de", "", join(', ', $aRecip)) . ".\n";
-  foreach($aRecip as $sTo) {
-    mail($sTo, $sSubj, $sMsg, $sHeader);
-  }
+if ($iErrorLevel > 0) {
+    $sSubj = basename(__FILE__) . ': error';
+    $sMsg = "An error occured while running the file " .
+            __FILE__ . ":\n\n" .
+            $sCmd . "\n\n" . $result . "\n\n" .
+            "Repo: $_REQUEST[repo] - " . `echo %cd%`;
+
+    $sHeader = "From: " . basename(__FILE__) . '@quodata.de';
+    $aRecip = array(
+        'Blaeul@quodata.de',
+        'Oertel@quodata.de',
+        'Sgorzaly@quodata.de',
+        'Oscar.Reinecke@quodata.de',
+    );
+    $sMsg .= "\nWeitere Details gibt es unter https://git04.quodata.de/it/$_REQUEST[repo].\n\n";
+    $sMsg .= "Diese E-Mail ging an " . str_replace("@quodata.de", "", join(', ', $aRecip)) . ".\n";
+    foreach ($aRecip as $sTo) {
+        mail($sTo, $sSubj, $sMsg, $sHeader);
+    }
 }
 die($iErrorLevel);
