bbf2f175e6db756e8f38f025c3cd4afa5d66e021
Screenshot01-PC <test@quodata.de>
2016-05-25 12:11:55 +0200

    trivialen Syntaxfehler behoben: git hatte eine Klammer gefressen

--- include/screenshot.inc.php
+++ include/screenshot.inc.php
@@ -112,6 +112,7 @@ function createDifferenceImage($sFileIst, $sFileSoll, $sStem) {
         if (filesize("$sStem-difference2.png") <  filesize("$sStem-difference.png")) {
            copy("$sStem-difference2.png", "$sStem-difference.png");
         }
+    }
     $sCmd = "$sCompare -compose src \"$sFileIst\" \"$sFileSoll\" \"$sStem-difference.png\"";
     $result = `$sCmd 2>&1`;
     if ($cropped) {
