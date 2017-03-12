11b29e7789e1da9a4179bc4ac23808df31ed8235
Christian Bläul <blaeul@quodata.de>
2017-03-12 11:53:54 +0100

    falls Sprache per GET-Parameter 'l' übergeben wurde, diese auch an zukünftig besuchte Seiten weitergeben

--- html/done.php
+++ html/done.php
@@ -1,10 +1,18 @@
 <?php
 
+// wird von Taste A aufgerufen, wenn ein Testergebnis zum neuen Sollzustand erklärt wird
+
 require '../include/screenshot.inc.php';
 require '../include/projectstatus.inc.php';
 
 getStatusOfAllProjects();
 
+$sLanguage = empty($_GET['l']) ? '' : '&l=' . $_GET['l'];
+$sMessage = '&message=' . urlencode(LANG == 'en' ?
+    'The result was saved as the new target state.' :
+    'Das Ergebnis wurde als neuer Sollwert abgelegt.');
+
 header('Location: index.php' .
   '?project=' . urlencode($_REQUEST['project']) .
-  '&message=' . urlencode('Das Ergebnis wurde als neuer Sollwert abgelegt.'));
+  $sLanguage .
+  $sMessage);
