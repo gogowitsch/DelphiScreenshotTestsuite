fead9744798a1d295085895d72568d9874b5c2e1
Christian Bläul <blaeul@quodata.de>
2017-03-16 13:30:27 +0100

    done.php: nur Kommentare hinzu

--- html/done.php
+++ html/done.php
@@ -7,7 +7,11 @@ require '../include/projectstatus.inc.php';
 
 getStatusOfAllProjects();
 
+// Parameter 'l' nur an den nächsten Request mitschicken, 
+// wenn wir ihn bereits bekommen haben:
 $sLanguage = empty($_GET['l']) ? '' : '&l=' . $_GET['l'];
+
+// die Konstante LANG wird abhängig vom Parameter 'l' und vom Host gesetzt.
 $sMessage = '&message=' . urlencode(LANG == 'en' ?
     'The result was saved as the new target state.' :
     'Das Ergebnis wurde als neuer Sollwert abgelegt.');
