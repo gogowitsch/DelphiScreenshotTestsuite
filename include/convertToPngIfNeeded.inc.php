326ed79234ab927613dae5d7e9d224ab6689a06a
Christian Bläul <blaeul@quodata.de>
2017-11-08 11:26:15 +0100

    convertToPngIfNeeded: if it fails, show more debug information.

--- include/convertToPngIfNeeded.inc.php
+++ include/convertToPngIfNeeded.inc.php
@@ -1,6 +1,14 @@
 <?php
 
-/****
+/**
+ * If the GET/POST parameter 'job_done' is present, all files are converted to 
+ * PNG. Otherwise only a few, to allow requests to the browser to finish 
+ * quickly.
+ * 
+ * @global int $iConvertedBmpsDuringThisCall
+ * 
+ * @param string $sName Path to file, without file extension
+ * @param string $sExt
  * @return string Neuer Dateiname
  */
 function convertToPngIfNeeded($sName, &$sExt) {
@@ -14,37 +22,24 @@ function convertToPngIfNeeded($sName, &$sExt) {
 
     if (stristr($sExt, 'png'))
         return $sName . '.png';
-    if ($iConvertedBmpsDuringThisCall > 3 && empty($_REQUEST['job_done']))
+    if ($iConvertedBmpsDuringThisCall > 3 && empty($_REQUEST['job_done'])) {
         return $sName . '.' . $sExt;
-
+    }
 
     if (!file_exists("../include/path.ini")) {
         $sUrl = 'http://screenshot01-pc/DelphiScreenshotTestsuite/include/path.ini';
         die("You must add include/path.ini,
           e.g. modified version from <a href='$sUrl' target=path>$sUrl</a>.");
     }
-
-    $aIni = parse_ini_file("../include/path.ini");
-
-    if (empty($aIni['CONVERT_PATH']))
-        die("CONVERT_PATH not set: You must specity the path to Imagemagick's convert.exe in include/path.ini");
-    else $sConvertPath = $aIni['CONVERT_PATH'];
-
-    if (empty($aIni['GS_PATH']))
-        die("GS_PATH not set: You must specity the path to ghostscript's gs.exe in include/path.ini");
-    else $sGsPath = $aIni['GS_PATH'];
-
-    $sConvert = "set path=%path%;$sGsPath; && \"$sConvertPath\\convert.exe\"";
-
+   
     $sRetVal = '';
 
     if (stristr($sExt, 'pdf')) {
         $sPath = str_replace("/", "\\", dirname($sName));
         $sBaseName = basename($sName);
         $sCmd = "cd $sPath && ";
-        // $sMagic = stristr($sExt, 'bmp') ? 'DIB:' : '';
-        $sFile = "\"$sBaseName.$sExt\"";
-        // echo $sMagic;
+        $sFile = "\"$sBaseName.$sExt\"";        
+        $sConvert = get_convert_cmd();
         $sCmd .= "$sConvert -density 75 -strip $sFile -append \"$sBaseName.png\"";
 
         #die( "$sCmd<hr>");
@@ -77,7 +72,31 @@ function convertToPngIfNeeded($sName, &$sExt) {
         return "$sName.png";
     }
 
-    if (!$sRetVal)
+    if (!$sRetVal) {
+        // Success! File was converted.
         return $sName . '.png';
-    echo "<h1>Fehler 547</h1><tt>$sCmd</tt><br><b style='color:red'>$sRetVal </b><hr>";
+    }
+    echo "<h1>Fehler 547 bei $sBaseName</h1>";
+    echo "<details open=true><summary>Command line</summary><tt>$sCmd</tt><br><b style='color:red'>$sRetVal </b></details>";
+    echo '<details><summary>dir</summary>' . nl2br(`cd $sPath && dir "$sBaseName.*"`) . '</details>';
+
+    echo '<details><summary>File content</summary>';
+    readfile("$sPath/$sBaseName.$sExt");
+    echo '</details>';
+}
+
+function get_convert_cmd() {
+    $aIni = parse_ini_file("../include/path.ini");
+
+    if (empty($aIni['CONVERT_PATH']))
+        die("CONVERT_PATH not set: You must specity the path to Imagemagick's convert.exe in include/path.ini");
+    else
+        $sConvertPath = $aIni['CONVERT_PATH'];
+
+    if (empty($aIni['GS_PATH']))
+        die("GS_PATH not set: You must specity the path to ghostscript's gs.exe in include/path.ini");
+    else
+        $sGsPath = $aIni['GS_PATH'];
+
+    return "set path=%path%;$sGsPath; && \"$sConvertPath\\convert.exe\"";
 }
